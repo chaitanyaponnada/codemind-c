ece17e31145ef262a9ce5997c370ad79110707d5
chaitanyaponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-01-03 21:17:57 +0530

    #include<stdio.h> int main() {  char c;  scanf("%c",&c);  switch(c)  {  case 'V':printf("violet");break;  case 'I':printf("Indigo");break;  case 'B':printf("Blue");break;  case 'G':printf("Green");break;  case 'Y':printf("Yellow");break; case 'O':printf("Orange");break;     case'R':printf("Red");break;     default:printf("-1"); } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ char c;
+ scanf("%c",&c);
+ switch(c)
+ {
+ case 'V':printf("violet");break;
+ case 'I':printf("Indigo");break;
+ case 'B':printf("Blue");break;
+ case 'G':printf("Green");break;
+ case 'Y':printf("Yellow");break;
+case 'O':printf("Orange");break;
+    case'R':printf("Red");break;
+    default:printf("-1");
+}
+}
