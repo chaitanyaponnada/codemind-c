4abb8cc7709ba5b33cbf9e108bac1f0ad7e02c9d
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-05-30 00:28:59 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n];     int s=0;     int i;     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++){         if(a[i]%2!=0){             s=s+a[i];         }     }     printf("%d",s); }

---
+++ Odd_Sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int s=0;
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        if(a[i]%2!=0){
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+}
