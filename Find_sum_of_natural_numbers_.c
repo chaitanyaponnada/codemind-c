223dffee5d742e15a7c9f800b58236e8789d99dc
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2023-06-06 01:24:42 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int s;     s=(n*(n+1))/2;     printf("%d",s); }

--- Find_sum_of_natural_numbers_.c
+++ Find_sum_of_natural_numbers_.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,s=0;
+int main(){
+    int n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        s=s+i;
-    }
+    int s;
+    s=(n*(n+1))/2;
     printf("%d",s);
 }
